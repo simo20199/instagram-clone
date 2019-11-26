03b7f3de452a6e91975a00e194f38ed6dd8afa8b
Mohammed Nouri <simoiroun25@gmail.com>
2019-11-26 11:00:08 +0100

    frontend part

--- app/models/account.rb
+++ app/models/account.rb
@@ -6,4 +6,8 @@ class Account < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
